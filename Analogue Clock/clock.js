19b5d8a417e644b2ca570a98c5710d5514bc156c
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-16 15:47:19 +0200

    create a canvas object fro HTML element

--- Analogue Clock/clock.js
+++ Analogue Clock/clock.js
@@ -1,1 +1,11 @@
 
+//create a canvas object fro HTML element
+var canvas = document.getElementById('canvas');
+//create a 2d drawing object
+var ctx = canvas.getContext('2d');
+//calculate the clock radius by using the height
+var radius = canvas.height / 2;
+//remap the x and y axis to the center of the canvas
+ctx.translate(radius, radius);
+//reduce clock radius by 90%
+radius = radius * 0.90;
