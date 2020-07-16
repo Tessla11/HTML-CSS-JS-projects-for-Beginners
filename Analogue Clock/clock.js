eb3573ab2b0a258db2542afdbb2b23f104369e69
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-16 15:48:51 +0200

    Update clock.js

--- Analogue Clock/clock.js
+++ Analogue Clock/clock.js
@@ -40,3 +40,20 @@ function drawFace(ctx, radius){
     ctx.fillStyle = '#333';
     ctx.fill();
 }
+function drawNumbers(ctx, radius) {
+    var ang;
+    var num;
+    ctx.font = radius*0.15 + "px arial"; //set font at 15% of radius
+    ctx.textBaseline = "middle"; //set text alignment to middle
+    ctx.textAlign = "center"; //set text alignment to center
+    for(num=1; num < 13; num++){ //calculate the print position for each number
+        ang = num *Math.PI /6;
+        ctx.rotate(ang);
+        ctx.translate(0, -radius*0.85);
+        ctx.rotate(-ang);
+        ctx.fillText(num.toString(), 0, 0);
+        ctx.rotate(ang);
+        ctx.translate(0, radius*0.85);
+        ctx.rotate(-ang);
+    }
+}
