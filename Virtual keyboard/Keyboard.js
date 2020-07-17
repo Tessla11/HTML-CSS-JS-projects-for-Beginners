9cd235097b2e04b3fbfd47a3753e1a8ad260b2d3
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-17 13:40:46 +0200

    Setup main elements

--- Virtual keyboard/Keyboard.js
+++ Virtual keyboard/Keyboard.js
@@ -19,3 +19,10 @@ const Keyboard = {
         // Create main elements
         this.elements.main = document.createElement("div");
         this.elements.keysContainer = document.createElement("div");
+
+        // Setup main elements
+        this.elements.main.classList.add("keyboard", "keyboard--hidden");
+        this.elements.keysContainer.classList.add("keyboard__keys");
+        this.elements.keysContainer.appendChild(this._createKeys());
+
+        this.elements.keys = this.elements.keysContainer.querySelectorAll(".keyboard__key");
