4be27b3b5e4c6bbe0cd311cbf39361e070150264
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-18 10:29:00 +0200

    generate Quote

--- Quote of Day Project/quote.js
+++ Quote of Day Project/quote.js
@@ -19,3 +19,10 @@ const arrayOfQuotes = [
      'quote': 'Do not take life too seriously. You will not get out alive.'
     },
 ];
+
+function generateQuote(){
+    const random = Number.parseInt(Math.random()*arrayOfQuotes.length + 1);
+    document.querySelector('#quoteOutput').textContent = `\"${arrayOfQuotes[random].quote}\"`;
+    document.querySelector('#authorOutput').textContent = `--${arrayOfQuotes[random].author}`;
+    
+}
