fb5d935521969a088617bd0b2356845bbbb5905d
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-21 09:49:25 +0200

    get distance current date to countdown date

--- Event Countdown Timer/app.js
+++ Event Countdown Timer/app.js
@@ -3,3 +3,6 @@ let countDownFromDate = new Date('Feb 1, 2020 12:00:00').toDateString();
 let countDownDate = new Date('Feb 1, 2020 12:00:00').getTime();
 let x = setInterval(function(){
 let now = new Date().getTime();
+
+//get distance between current date to countdown date
+let distance = countDownDate - now;
