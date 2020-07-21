ac21fc6ca09202a5040b90eb58f464935666a8cf
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-21 09:48:41 +0200

    Set date to countdown to

---
+++ Event Countdown Timer/app.js
@@ -0,0 +1,5 @@
+//Set date to countdown to:
+let countDownFromDate = new Date('Feb 1, 2020 12:00:00').toDateString();
+let countDownDate = new Date('Feb 1, 2020 12:00:00').getTime();
+let x = setInterval(function(){
+let now = new Date().getTime();
