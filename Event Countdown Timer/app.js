9dfca8ab01147dd69986f18b4c6bd1388688e5fb
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-21 09:50:15 +0200

    output countdown from date

--- Event Countdown Timer/app.js
+++ Event Countdown Timer/app.js
@@ -12,3 +12,7 @@ let days = Math.floor(distance / (1000 * 60 *60 * 24));
 let hours = Math.floor((distance % (1000 * 60 *60*24)) / (1000 * 60 * 60));
 let minutes = Math.floor((distance % (1000 * 60 * 60))/ (1000 * 60));
 let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+
+//output countdown from date
+document.getElementById('countdown-from').innerHTML = `${countDownFromDate}`
