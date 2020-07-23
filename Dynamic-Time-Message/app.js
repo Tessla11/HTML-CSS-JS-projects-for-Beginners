e586d2cba41fbae68e834d36ac9364907d8cb4f5
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-23 17:30:41 +0200

    select the message area from the DOM

--- Dynamic-Time-Message/app.js
+++ Dynamic-Time-Message/app.js
@@ -6,3 +6,6 @@ const hour = date.getHours();
 
 //select the button from the DOM
 const button = document.querySelector('button');
+
+//select the message area from the DOM
+const messageArea = document.querySelector('#message');
