04e70e9ccf8080bd5ef423b367c7e8e5e196f7fb
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-24 11:30:52 +0200

    Selection items from the DOM

---
+++ grocery-list-todo-project/app.js
@@ -0,0 +1,5 @@
+//Selection items from the DOM
+//Add items container
+const addItemsAction = document.querySelector('.addItems-action');
+const input = document.querySelector('.addItems-input');
+const submit = document.querySelector('.addItems-submit');
