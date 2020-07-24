53ecc8e272d83442966c866bf5cd298f7174a2a0
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-24 11:31:17 +0200

    Display items container

--- grocery-list-todo-project/app.js
+++ grocery-list-todo-project/app.js
@@ -3,3 +3,8 @@
 const addItemsAction = document.querySelector('.addItems-action');
 const input = document.querySelector('.addItems-input');
 const submit = document.querySelector('.addItems-submit');
+
+//Display items container
+const list = document.querySelector('.grocery-list');
+const displayItemsAction = document.querySelector('.displayItems-action');
+const clear = document.querySelector('.displayItems-clear');
