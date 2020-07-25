c3c1d3426a6dbc6d98ba130a2f45709e1fe5d7e0
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-25 11:26:43 +0200

    update

--- Insert-To-MySQL-DB/index.php
+++ Insert-To-MySQL-DB/index.php
@@ -43,3 +43,18 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
 }
 
 ?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+  <link rel="stylesheet" href="style.css">
+  <title>Contact Us</title>
+</head>
