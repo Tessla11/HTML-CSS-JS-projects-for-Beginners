f281c7a81e9627db128613ec27eff4564909b82b
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-27 11:40:50 +0200

    display numbers on the canvas

--- Math Addition App Project/app.js
+++ Math Addition App Project/app.js
@@ -5,3 +5,10 @@ let secondNumber = parseInt(Math.random()*10);
 
 //get the total
 let total = firstNumber + secondNumber;
+
+//display numbers on the canvas
+let primary = document.getElementById('primary-number');
+    primary.innerHTML = `<p>${firstNumber}</p>`;
+
+let secondary = document.getElementById('secondary-number');
+    secondary.innerHTML = `<p>${secondNumber}</p>`
