1bba73e217dc6a127bfb90ecc6defd62cee5a7ed
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-28 20:03:46 +0200

    Show prev

--- Full Screen image slider/script.js
+++ Full Screen image slider/script.js
@@ -15,3 +15,10 @@ function startSlide() {
     reset();
     sliderImages[0].style.display = "block";
   }
+
+  // Show prev
+function slideLeft() {
+    reset();
+    sliderImages[current - 1].style.display = "block";
+    current--;
+  }
