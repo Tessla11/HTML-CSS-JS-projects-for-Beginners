51e7c7faeaf0d238dcfb25a9b1689d453a92ad2b
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-28 20:11:37 +0200

    Convert to an array

--- DOM Item Lister/script.js
+++ DOM Item Lister/script.js
@@ -57,3 +57,14 @@ function filterItems(e){
     var text = e.target.value.toLowerCase();
     // Get lis
     var items = itemList.getElementsByTagName('li');
+
+    // Convert to an array
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
