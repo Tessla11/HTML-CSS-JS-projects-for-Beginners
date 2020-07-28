de2c0d2cd1599614d8d6e5c62fd9439079027279
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-07-28 20:10:21 +0200

    Form submit event

---
+++ DOM Item Lister/script.js
@@ -0,0 +1,10 @@
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
+
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
