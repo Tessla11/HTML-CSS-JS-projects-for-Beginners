6d3196a68159f01154255b4a9b5ef147bf436beb
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-01 15:20:37 +0200

    Generate a random number between 1 and 500

---
+++ Number Guessing Game/app.js
@@ -0,0 +1,21 @@
+//Generate a random number between 1 and 500
+let randomNumber = parseInt((Math.random()*100)+1);
+const submit = document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const startOver = document.querySelector('.resultParas');
+const lowOrHi = document.querySelector('.lowOrHi');
+const p = document.createElement('p');
+let previousGuesses = [];
+let numGuesses = 1;
+let playGame = true;
+
+if (playGame){
+    subt.addEventListener('click', function(e){
+        e.preventDefault();
+        //Grab guess from user
+        const guess = parseInt(userInput.value);
+        validateGuess(guess);
+    });
+}
