f81bfa6f2255ca1ac6d87ed114910c152c1bb186
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-03 17:08:30 +0200

    Set up the slider

---
+++ Image-Slider/main.js
@@ -0,0 +1,10 @@
+  
+const slideImage = document.querySelectorAll(".slide-image");
+const slidesContainer = document.querySelector(".slides-container");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+const navigationDots = document.querySelector(".navigation-dots");
+
+let numberOfImages = slideImage.length;
+let slideWidth = slideImage[0].clientWidth;
+let currentSlide = 0;
