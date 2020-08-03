ff1ca9792c4e005f505e31183ef819ac169ac357
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-03 17:09:10 +0200

    set up slider

--- Image-Slider/main.js
+++ Image-Slider/main.js
@@ -8,3 +8,22 @@ const navigationDots = document.querySelector(".navigation-dots");
 let numberOfImages = slideImage.length;
 let slideWidth = slideImage[0].clientWidth;
 let currentSlide = 0;
+
+
+function init() {
+    /*   
+      slideImage[0] = 0
+      slideImage[1] = 100%
+      slideImage[2] = 200%
+      */
+  
+    slideImage.forEach((img, i) => {
+      img.style.left = i * 100 + "%";
+    });
+  
+    slideImage[0].classList.add("active");
+  
+    createNavigationDots();
+  }
+  
+  init();
