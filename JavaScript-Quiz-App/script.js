1882a0c837b99aeb10dd13d95791d02dc58a61d3
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-09 14:03:17 +0200

    function selectAnswer

--- JavaScript-Quiz-App/script.js
+++ JavaScript-Quiz-App/script.js
@@ -38,3 +38,27 @@ function showQuestion(question) {
         answerButtonsElement.appendChild(button)
     })
 }
+
+
+function resetState() {
+    clearStatusClass(document.body)
+    nextButton.classList.add('hide')
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    }
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide')
+    } else {
+        startButton.innerText = 'Restart'
+        startButton.classList.remove('hide')
+    }
+}
