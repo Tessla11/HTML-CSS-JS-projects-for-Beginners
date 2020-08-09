d0e58ff23ceebb4e60f376f483b562cf301f6d7e
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-09 14:02:54 +0200

    function showQuestion

--- JavaScript-Quiz-App/script.js
+++ JavaScript-Quiz-App/script.js
@@ -24,3 +24,17 @@ function setNextQuestion() {
     resetState()
     showQuestion(shuffledQuestions[currentQuestionIndex])
 }
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
