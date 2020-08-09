e08990ad8c0ec1462da9dae33313dadca80f8076
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-09 14:03:49 +0200

    const questions

--- JavaScript-Quiz-App/script.js
+++ JavaScript-Quiz-App/script.js
@@ -62,3 +62,57 @@ function selectAnswer(e) {
         startButton.classList.remove('hide')
     }
 }
+
+
+
+
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+        element.classList.add('correct')
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+}
+
+
+
+const questions = [{
+        question: 'What is 2 + 2?',
+        answers: [
+            { text: '4', correct: true },
+            { text: '22', correct: false }
+        ]
+    },
+    {
+        question: 'Who is the best YouTuber?',
+        answers: [
+            { text: 'Web Dev Simplified', correct: true },
+            { text: 'Traversy Media', correct: true },
+            { text: 'Dev Ed', correct: true },
+            { text: 'Fun Fun Function', correct: true }
+        ]
+    },
+    {
+        question: 'Is web development fun?',
+        answers: [
+            { text: 'Kinda', correct: false },
+            { text: 'YES!!!', correct: true },
+            { text: 'Um no', correct: false },
+            { text: 'IDK', correct: false }
+        ]
+    },
+    {
+        question: 'What is 4 * 2?',
+        answers: [
+            { text: '6', correct: false },
+            { text: '8', correct: true }
+        ]
+    }
+]
