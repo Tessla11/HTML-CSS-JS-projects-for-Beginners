a2e6b7b8325a81624ee6bce86450d1abb01184e2
gregoryann <32854050+gregoryann@users.noreply.github.com>
2020-08-09 14:01:50 +0200

    ddEventListener

---
+++ JavaScript-Quiz-App/script.js
@@ -0,0 +1,13 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
+
+startButton.addEventListener('click', startGame)
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++
+    setNextQuestion()
+})
